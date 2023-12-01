fbee06ca2ea3d4fdaa90721c8a528aff1324ae42
wmeziane01 <wmeziane01@educarex.es>
2023-12-01 14:02:41 +0100

    sdtg

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola Mundo");
-		System.out.println("Buenis días");
+		System.out.println("Buenifs días");
 		
 	}
 
